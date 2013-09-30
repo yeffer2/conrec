05dc0ac2b3a39b5b153af9e4097de8d576c46f71
cdainfo.michel@gmail.com <cdainfo.michel@gmail.com@c82d28a0-95d9-8df2-09f9-27b2531fcb27>
2013-09-30 22:49:51 +0000

    Se inicia la construcción del proyecto de scripts. Se crean los usuarios requeridos. Se termina la importación de las tablas de reuso de Harec.

---
+++ reniec-scripts/create/tables/create-tables.sql
@@ -0,0 +1,337 @@
+
+/*==============================================================*/
+/* Table: CFG_LISTA                                             */
+/*==============================================================*/
+create table CONREC.CFG_LISTA 
+(
+   ID                   NUMBER(8)            not null,
+   CODIGO               NVARCHAR2(20)        not null,
+   NOMBRE               NVARCHAR2(50)        not null,
+   DESCRIPCION          NVARCHAR2(250),
+   ESTADO               NCHAR(1)             not null,
+   CREADOR              NUMBER(16)           not null,
+   CREACION             TIMESTAMP            not null,
+   EDITOR               NUMBER(16),
+   EDICION              TIMESTAMP
+);
+
+alter table CONREC.CFG_LISTA
+   add constraint PK_CFG_LISTA primary key (ID);
+
+/*==============================================================*/
+/* Index: UNQ_CODIGO_LISTA                                      */
+/*==============================================================*/
+create unique index CONREC.UNQ_CODIGO_LISTA on CONREC.CFG_LISTA (
+   CODIGO ASC
+);
+
+grant INSERT,SELECT,UPDATE,DELETE on CONREC.CFG_LISTA to APP_RENIEC;
+
+/*==============================================================*/
+/* Table: CFG_PARAMETRO                                         */
+/*==============================================================*/
+create table CONREC.CFG_PARAMETRO 
+(
+   CODIGO               NVARCHAR2(15)        not null,
+   NOMBRE               NVARCHAR2(100)       not null,
+   DESCRIPCION          NVARCHAR2(250),
+   VALOR                NVARCHAR2(500)       not null,
+   ESTADO               NUMBER(16)           not null,
+   CREADOR              NUMBER(16)           not null,
+   CREACION             TIMESTAMP            not null,
+   EDITOR               NUMBER(16),
+   EDICION              TIMESTAMP
+);
+
+alter table CONREC.CFG_PARAMETRO
+   add constraint PK_CFG_PARAMETRO primary key (CODIGO);
+
+grant DELETE,INSERT,SELECT,UPDATE on CONREC.CFG_PARAMETRO to APP_RENIEC;
+
+/*==============================================================*/
+/* Table: CFG_VALOR                                             */
+/*==============================================================*/
+create table CONREC.CFG_VALOR 
+(
+   ID                   NUMBER(16)           not null,
+   LISTA                NUMBER(8)            not null,
+   CODIGO               NVARCHAR2(15)        not null,
+   NOMBRE               NVARCHAR2(100)       not null,
+   ORDEN                NUMBER(8),
+   DESCRIPCION          NVARCHAR2(250),
+   ESTADO               NCHAR(1)             not null,
+   CREADOR              NUMBER(16)           not null,
+   CREACION             TIMESTAMP            not null,
+   EDITOR               NUMBER(16),
+   EDICION              TIMESTAMP
+);
+
+alter table CONREC.CFG_VALOR
+   add constraint PK_CFG_VALOR primary key (ID);
+
+/*==============================================================*/
+/* Index: UNQ_CODIGO_VALOR                                      */
+/*==============================================================*/
+create unique index CONREC.UNQ_CODIGO_VALOR on CONREC.CFG_VALOR (
+   CODIGO ASC,
+   LISTA ASC
+);
+
+grant DELETE,INSERT,SELECT,UPDATE on CONREC.CFG_VALOR to APP_RENIEC;
+
+/*==============================================================*/
+/* Table: SEG_MEMBRESIA                                         */
+/*==============================================================*/
+create table CONREC.SEG_MEMBRESIA 
+(
+   ID                   NUMBER(16)           not null,
+   ROL                  NUMBER(8)            not null,
+   TIPO                 NUMBER(16)           not null,
+   VALOR                NVARCHAR2(15)        not null,
+   ESTADO               NUMBER(16)           not null,
+   CREADOR              NUMBER(16)           not null,
+   CREACION             TIMESTAMP            not null,
+   EDITOR               NUMBER(16),
+   EDICION              TIMESTAMP
+);
+
+alter table CONREC.SEG_MEMBRESIA
+   add constraint PK_SEG_MEMBRESIA primary key (ID);
+
+alter table CONREC.SEG_MEMBRESIA
+   add constraint AK_UNQ_MEMBRESIA_REGI_SEG_MEMB unique (TIPO, VALOR);
+
+grant DELETE,INSERT,SELECT,UPDATE on CONREC.SEG_MEMBRESIA to APP_RENIEC;
+
+/*==============================================================*/
+/* Table: SEG_OPCION                                            */
+/*==============================================================*/
+create table CONREC.SEG_OPCION 
+(
+   ID                   NUMBER(8)            not null,
+   PADRE                NUMBER(8),
+   CODIGO               NVARCHAR2(35)        not null,
+   NOMBRE               NVARCHAR2(50)        not null,
+   DESCRIPCION          NVARCHAR2(250),
+   ORDEN                NUMBER(8),
+   TIPO                 NUMBER(16)           not null,
+   ESTADO               NUMBER(16)           not null,
+   CREADOR              NUMBER(16)           not null,
+   CREACION             TIMESTAMP            not null,
+   EDITOR               NUMBER(16),
+   EDICION              TIMESTAMP
+);
+
+alter table CONREC.SEG_OPCION
+   add constraint PK_SEG_OPCION primary key (ID);
+
+/*==============================================================*/
+/* Index: UNQ_CODIGO_OPCION                                     */
+/*==============================================================*/
+create unique index CONREC.UNQ_CODIGO_OPCION on CONREC.SEG_OPCION (
+   CODIGO ASC
+);
+
+grant DELETE,INSERT,SELECT,UPDATE on CONREC.SEG_OPCION to APP_RENIEC;
+
+/*==============================================================*/
+/* Table: SEG_PERMISO                                           */
+/*==============================================================*/
+create table CONREC.SEG_PERMISO 
+(
+   ID                   NUMBER(16)           not null,
+   OPCION               NUMBER(8)            not null,
+   ROL                  NUMBER(8)            not null,
+   ESTADO               NUMBER(16)           not null,
+   CREADOR              NUMBER(16)           not null,
+   CREACION             TIMESTAMP            not null,
+   EDITOR               NUMBER(16),
+   EDICION              TIMESTAMP
+);
+
+alter table CONREC.SEG_PERMISO
+   add constraint PK_SEG_PERMISO primary key (ID);
+
+grant DELETE,INSERT,SELECT,UPDATE on CONREC.SEG_PERMISO to APP_RENIEC;
+
+/*==============================================================*/
+/* Table: SEG_ROL                                               */
+/*==============================================================*/
+create table CONREC.SEG_ROL 
+(
+   ID                   NUMBER(8)            not null,
+   PADRE                NUMBER(8),
+   CODIGO               NVARCHAR2(10),
+   NOMBRE               NVARCHAR2(30),
+   DESCRIPCION          NVARCHAR2(250),
+   ESTADO               NUMBER(16)           not null,
+   CREADOR              NUMBER(16),
+   CREACION             TIMESTAMP            not null,
+   EDITOR               NUMBER(16),
+   EDICION              TIMESTAMP
+);
+
+alter table CONREC.SEG_ROL
+   add constraint PK_SEG_ROL primary key (ID);
+
+/*==============================================================*/
+/* Index: UNQ_CODIGO_ROL                                        */
+/*==============================================================*/
+create index CONREC.UNQ_CODIGO_ROL on CONREC.SEG_ROL (
+   CODIGO ASC
+);
+
+grant DELETE,INSERT,SELECT,UPDATE on CONREC.SEG_ROL to APP_RENIEC;
+	
+/*==============================================================*/
+/* Table: SEG_USUARIO                                           */
+/*==============================================================*/
+create table CONREC.SEG_USUARIO 
+(
+   ID                   NUMBER(16)           not null,
+   NOMBRES              NVARCHAR2(200)       not null,
+   PATERNO              NVARCHAR2(150)       not null,
+   MATERNO              NVARCHAR2(150)       not null,
+   REGISTRO             NCHAR(7)             not null,
+   CLAVE                NCHAR(10)            not null,
+   CARGO                NVARCHAR2(4)         not null,
+   DESC_CARGO           NVARCHAR2(45),
+   CORREO               NVARCHAR2(45),
+   OFICINA              NVARCHAR2(4)         not null,
+   DESC_OFICINA         NVARCHAR2(45),
+   CREADOR              NUMBER(16),
+   CREACION             TIMESTAMP            not null,
+   EDITOR               NUMBER(16),
+   EDICION              TIMESTAMP
+);
+
+alter table CONREC.SEG_USUARIO
+   add constraint PK_SEG_USUARIO primary key (ID);
+
+/*==============================================================*/
+/* Index: UNQ_USUARIO                                           */
+/*==============================================================*/
+create unique index CONREC.UNQ_USUARIO on CONREC.SEG_USUARIO (
+   REGISTRO ASC
+);
+
+grant DELETE,INSERT,SELECT,UPDATE on CONREC.SEG_USUARIO to APP_RENIEC;
+
+/*==============================================================*/
+/* Table: SEQ_ENTIDAD                                           */
+/*==============================================================*/
+create table CONREC.SEQ_ENTIDAD 
+(
+   ENTIDAD              NVARCHAR2(250)       not null,
+   ULTIMO_ID            NUMBER(16)           not null
+);
+
+alter table CONREC.SEQ_ENTIDAD
+   add constraint PK_SEQ_ENTIDAD primary key (ENTIDAD);
+
+grant DELETE,INSERT,SELECT,UPDATE on CONREC.SEQ_ENTIDAD to APP_RENIEC;
+
+
+alter table CONREC.CFG_LISTA
+   add constraint FK_CGF_LISTA_CREADOR foreign key (CREADOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.CFG_LISTA
+   add constraint FK_CGF_LISTA_EDITOR foreign key (EDITOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.CFG_PARAMETRO
+   add constraint FK_CFG_PARAMETRO_CREADOR foreign key (CREADOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.CFG_PARAMETRO
+   add constraint FK_CFG_PARAMETRO_EDITOR foreign key (EDITOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.CFG_PARAMETRO
+   add constraint FK_CFG_PARAMETRO_ESTADO foreign key (ESTADO)
+      references CONREC.CFG_VALOR (ID);
+
+alter table CONREC.SEG_MEMBRESIA
+   add constraint FK_SEG_MEMBRESIA_CREADOR foreign key (CREADOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.SEG_MEMBRESIA
+   add constraint FK_SEG_MEMBRESIA_EDITOR foreign key (EDITOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.SEG_MEMBRESIA
+   add constraint FK_SEG_MEMBRESIA_ESTADO foreign key (ESTADO)
+      references CONREC.CFG_VALOR (ID);
+
+alter table CONREC.SEG_MEMBRESIA
+   add constraint FK_SEG_MEMBRESIA_SEG_ROL foreign key (ROL)
+      references CONREC.SEG_ROL (ID);
+
+alter table CONREC.SEG_MEMBRESIA
+   add constraint FK_SEG_MEMBRESIA_TIPO foreign key (TIPO)
+      references CONREC.CFG_VALOR (ID);
+
+alter table CONREC.SEG_OPCION
+   add constraint FK_OPCION_ESTADO foreign key (ESTADO)
+      references CONREC.CFG_VALOR (ID);
+
+alter table CONREC.SEG_OPCION
+   add constraint FK_SEG_OPCION_CREADOR foreign key (CREADOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.SEG_OPCION
+   add constraint FK_SEG_OPCION_EDITOR foreign key (EDITOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.SEG_OPCION
+   add constraint FK_SEG_OPCION_PADRE foreign key (PADRE)
+      references CONREC.SEG_OPCION (ID);
+
+alter table CONREC.SEG_OPCION
+   add constraint FK_SEG_OPCION_TIPO foreign key (TIPO)
+      references CONREC.CFG_VALOR (ID);
+
+alter table CONREC.SEG_PERMISO
+   add constraint FK_SEG_PERMISO_CREADOR foreign key (CREADOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.SEG_PERMISO
+   add constraint FK_SEG_PERMISO_EDITOR foreign key (EDITOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.SEG_PERMISO
+   add constraint FK_SEG_PERM_FK_SEG_PE_CFG_VALO foreign key (ESTADO)
+      references CONREC.CFG_VALOR (ID);
+
+alter table CONREC.SEG_PERMISO
+   add constraint FK_SEG_PERMISO_SEG_OPCION foreign key (OPCION)
+      references CONREC.SEG_OPCION (ID);
+
+alter table CONREC.SEG_PERMISO
+   add constraint FK_SEG_PERMISO_SEG_ROL foreign key (ROL)
+      references CONREC.SEG_ROL (ID);
+
+alter table CONREC.SEG_ROL
+   add constraint FK_SEG_ROL_CREADOR foreign key (CREADOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.SEG_ROL
+   add constraint FK_SEG_ROL_EDITOR foreign key (EDITOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.SEG_ROL
+   add constraint FK_SEG_ROL_ESTADO foreign key (ESTADO)
+      references CONREC.CFG_VALOR (ID);
+
+alter table CONREC.SEG_ROL
+   add constraint FK_SEG_ROL_PADRE foreign key (PADRE)
+      references CONREC.SEG_ROL (ID);
+
+alter table CONREC.SEG_USUARIO
+   add constraint FK_USUARIO_CREADOR foreign key (CREADOR)
+      references CONREC.SEG_USUARIO (ID);
+
+alter table CONREC.SEG_USUARIO
+   add constraint FK_USUARIO_EDITOR foreign key (EDITOR)
+      references CONREC.SEG_USUARIO (ID);
