8711cf869e15371f9c3f01dbeb81e9aa3e8b00d6
essenwanger@gmail.com <essenwanger@gmail.com@c82d28a0-95d9-8df2-09f9-27b2531fcb27>
2013-10-21 15:32:58 +0000

    Script con esquema RENIEC

--- trunk/reniec-scripts/data/02-quartz.sql
+++ trunk/reniec-scripts/data/02-quartz.sql
@@ -1,7 +1,7 @@
-INSERT INTO CONREC.qrtz_locks values('TRIGGER_ACCESS');
-INSERT INTO CONREC.qrtz_locks values('JOB_ACCESS');
-INSERT INTO CONREC.qrtz_locks values('CALENDAR_ACCESS');
-INSERT INTO CONREC.qrtz_locks values('STATE_ACCESS');
-INSERT INTO CONREC.qrtz_locks values('MISFIRE_ACCESS');
+INSERT INTO RENIEC.qrtz_locks values('TRIGGER_ACCESS');
+INSERT INTO RENIEC.qrtz_locks values('JOB_ACCESS');
+INSERT INTO RENIEC.qrtz_locks values('CALENDAR_ACCESS');
+INSERT INTO RENIEC.qrtz_locks values('STATE_ACCESS');
+INSERT INTO RENIEC.qrtz_locks values('MISFIRE_ACCESS');
 
 commit;
