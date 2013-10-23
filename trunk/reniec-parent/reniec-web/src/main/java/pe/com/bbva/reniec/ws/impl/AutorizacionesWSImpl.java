4eb8cfc0d795fd9545acd06606ba097c9ad074fd
cdainfo.michel@gmail.com <cdainfo.michel@gmail.com@c82d28a0-95d9-8df2-09f9-27b2531fcb27>
2013-10-23 22:23:42 +0000

    Se termina con el comportamiento del WS Autorizacion

--- trunk/reniec-parent/reniec-web/src/main/java/pe/com/bbva/reniec/ws/impl/AutorizacionesWSImpl.java
+++ trunk/reniec-parent/reniec-web/src/main/java/pe/com/bbva/reniec/ws/impl/AutorizacionesWSImpl.java
@@ -1,23 +1,37 @@
 package pe.com.bbva.reniec.ws.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.grupobbva.pe.simr.service.message.UsuarioResponse;
+
 import pe.com.bbva.reniec.dominio.Consultante;
+import pe.com.bbva.reniec.dominio.Valor;
+import pe.com.bbva.reniec.negocio.ConfiguracionService;
 import pe.com.bbva.reniec.negocio.ConsultantesService;
+import pe.com.bbva.reniec.persistencia.hibernate.BaseHibernate;
+import pe.com.bbva.reniec.utileria.Constante;
 import pe.com.bbva.reniec.utileria.Inject;
 import pe.com.bbva.reniec.ws.AutorizacionesWS;
 import pe.com.bbva.reniec.ws.dominio.ConsultanteWS;
 import pe.com.bbva.reniec.ws.dominio.RespuestaAutorizacion;
 
 public class AutorizacionesWSImpl implements AutorizacionesWS {
+	
+	protected final static Log logger = LogFactory.getLog(AutorizacionesWSImpl.class);
 
 	@Autowired
 	ConsultantesService consultantesService;
-	
+
+	@Autowired
+	ConfiguracionService configuracionService;
+
 	public ConsultanteWS consultarAutorizacion(String identificador) {
-		Consultante consultanteLocal = getConsultantesService().obtenerConsultantePorIdentificador(identificador);
-		ConsultanteWS  consultante = new ConsultanteWS();
-		if (consultanteLocal != null){
+		Consultante consultanteLocal = getConsultantesService()
+				.obtenerConsultantePorIdentificador(identificador);
+		ConsultanteWS consultante = new ConsultanteWS();
+		if (consultanteLocal != null) {
 			consultante.setIdentificador(consultanteLocal.getIdentificador());
 			consultante.setCodigo(consultanteLocal.getCodigoReniec());
 			consultante.setDOI(consultanteLocal.getDoi());
@@ -26,21 +40,66 @@ public class AutorizacionesWSImpl implements AutorizacionesWS {
 			consultante.setMaterno(consultanteLocal.getMaterno());
 			consultante.setNacimiento(consultanteLocal.getNacimiento());
 			if (consultanteLocal.getTipoDOI() != null)
-				consultante.setTipoDOI(consultanteLocal.getTipoDOI().getCodigo());
+				consultante.setTipoDOI(consultanteLocal.getTipoDOI()
+						.getCodigo());
 			if (consultanteLocal.getNacionalidad() != null)
-				consultante.setNacionalidad(consultanteLocal.getNacionalidad().getCodigo());
+				consultante.setNacionalidad(consultanteLocal.getNacionalidad()
+						.getCodigo());
 			consultante.setCentro(consultanteLocal.getCentro());
-			if(consultanteLocal.getOrigen() != null)
+			if (consultanteLocal.getOrigen() != null)
 				consultante.setOrigen(consultanteLocal.getOrigen().getCodigo());
-			if(consultanteLocal.getEstado() != null)
-				consultante.setEstado(consultanteLocal.getEstado().getCodigo());			
+			if (consultanteLocal.getEstado() != null)
+				consultante.setEstado(consultanteLocal.getEstado().getCodigo());
 		}
-		
+
 		return consultante;
 	}
 
-	public RespuestaAutorizacion registrarAutorizacion(ConsultanteWS consultante) {
+	public RespuestaAutorizacion registrarAutorizacion(
+			ConsultanteWS consultanteLocal) {
+		
 		RespuestaAutorizacion respuesta = new RespuestaAutorizacion();
+		try{
+		Consultante consultante = new Consultante();
+		consultante.setIdentificador(consultanteLocal.getIdentificador());
+		consultante.setCodigoReniec(consultanteLocal.getCodigo());
+		consultante.setDoi(consultanteLocal.getDOI());
+		consultante.setNombres(consultanteLocal.getNombres());
+		consultante.setPaterno(consultanteLocal.getPaterno());
+		consultante.setMaterno(consultanteLocal.getMaterno());
+		consultante.setNacimiento(consultanteLocal.getNacimiento());
+		consultante.setCentro(consultanteLocal.getCentro());
+		Valor tipoDoi = getConfiguracionService().obtenerValorxCodigo(
+				Constante.LISTA.CODIGO.DOI_TIPO, consultanteLocal.getTipoDOI());
+		Valor nacionalidad = getConfiguracionService().obtenerValorxCodigo(
+				Constante.LISTA.CODIGO.NACIONALIDAD_TIPO,
+				consultanteLocal.getNacionalidad());
+		Valor origen = getConfiguracionService().obtenerValorxCodigo(
+				Constante.LISTA.CODIGO.ORIGEN, consultanteLocal.getOrigen());
+		consultante.setTipoDOI(tipoDoi);
+		consultante.setNacionalidad(nacionalidad);
+		consultante.setOrigen(origen);
+
+		Consultante consultanteConId = getConsultantesService()
+				.obtenerConsultantePorIdentificador(
+						consultanteLocal.getIdentificador());
+		if (consultanteConId != null)
+			consultante.setId(consultanteConId.getId());
+
+		UsuarioResponse usuarioModificado = consultantesService
+				.guardarConsultanteWS(consultante, consultanteLocal.getAccion());
+
+		
+		respuesta.setError(usuarioModificado.getRefResponseHeader()
+				.getCodigoRespuesta());
+		respuesta.setMensaje(usuarioModificado.getRefResponseHeader()
+				.getMensajeRespuesta());
+		}
+		catch (Exception e){
+			logger.error(e.getStackTrace());
+			respuesta.setError("Error");
+			respuesta.setMensaje(e.getStackTrace().toString());			
+		}
 		return respuesta;
 	}
 
@@ -48,7 +107,12 @@ public class AutorizacionesWSImpl implements AutorizacionesWS {
 		if (consultantesService == null)
 			Inject.inject(this);
 		return consultantesService;
-	}	
-	
-	
+	}
+
+	private ConfiguracionService getConfiguracionService() {
+		if (configuracionService == null)
+			Inject.inject(this);
+		return configuracionService;
+	}
+
 }
